5551ba0b23bc9a24fbde2488fc18333c52976a78
soni895 <darshansoni895@gmail.com>
2023-07-29 23:42:25 +0530

    routes completed

--- index.js
+++ index.js
@@ -4,16 +4,13 @@ app.use(express.json());
 require("dotenv").config();
 port=process.env.port;
 const {db_connection}=require("./config/database");
-const user=require("./routes/user.js");
+const user=require("./routes/user");
 app.listen(port,()=>
 {
     console.log("hi darshan soni",port);
 });
 db_connection();
 app.use("/user",user);
-
-
-
 
 app.get("/",(req,res)=>
 
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
-const express=require("express");
-const router=express.Router();
-
+const express = require('express');
+const router = express.Router();
+const{login}=require("../controllers/login");
+const {signup}=require("../controllers/signup");
 router.post("/login",login);
 router.post("/signup",signup);
 module.exports=router;
