4662610f179fb0d852332a89afb17916679b04aa
soni895 <darshansoni895@gmail.com>
2023-07-30 13:45:00 +0530

    schema completed

--- models/usermodel.js
+++ models/usermodel.js
@@ -1,8 +1,30 @@
 const mongoose=require("mongoose");
 const userSchema= new mongoose.Schema(
     {
+        Name:{
+            type:String,
+            require:true,
+            trim:true,
+        },
 
+        Email:{
+            type:String,
+            require:true,
+            trim:true,
+        },
+        Password:{
+            type:String,
+            require:true,
+            trim:true,
+        },
+        Role:
+        {
+            type:String,
+            enum:["Admin","Student","Visitor"],
+         
+        }
+    
     }
 );
-const user=mongoose.model(user,userSchema);
+const user=mongoose.model("user",userSchema);
 module.exports=user;
