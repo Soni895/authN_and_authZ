d85382f99d3313d23493fb03765a95e228741d7b
soni895 <darshansoni895@gmail.com>
2023-07-30 23:00:37 +0530

    changes

--- controllers/login.js
+++ controllers/login.js
@@ -3,6 +3,53 @@ const bcrypt=require("bcrypt");
 const user=require("../models/usermodel");
 exports.login=async(req,res)=>
 {
+    try {
+        
+const {Email,Password}=req.body;
+if(Email&&Password)
+{
+    const response= await user.find({Email});
+    if(response)
+    {
+        const ismatch= await bcrypt.compare(Password,response.Password);
+        if(ismatch)
+        {
+
+        }
+        else{
+            res.status(400).json({
+                status:"unsuccessful",
+                error:"Password not match",
+            });
+
+        }
+    }
+
+    else{
+        res.status(400).json({
+            status:"unsuccessful",
+            error:"user not found",
+        });
+
+    }
+
+}
+else
+{
+    res.status(400).json({
+        status:"unsuccessful",
+        error:"empty field",
+    });
+}
+
+    } catch (error) {
+        res.status(400).json({
+            status:"unsuccessful",
+            error:error,
+        });
+
+        
+    }
 
   
   
