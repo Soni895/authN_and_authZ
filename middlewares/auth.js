b69de813752c15087e9ec10c9a2b7711102ea843
soni895 <darshansoni895@gmail.com>
2023-08-01 19:28:11 +0530

    auth complete but role not visible

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,1 +1,102 @@
 // auth isstudent isadmin
+
+const  jwt=require("jsonwebtoken");
+require("dotenv").config();
+const  jwt_secret=process.env.jwt_secret;
+exports.Auth=(req,res,next)=>
+{
+    try {
+        // extract jwt token with different way headerSize,body,cookies
+        //cookee parseer
+        const {token}=req.body;
+        if(!token)
+        {
+            return res.status(500).json(
+                {
+                    status:"unsuccessful",
+                    success:false,
+                    error:" token not found",
+                });
+        }
+        // verify the token
+        try {
+            const decode=jwt.verify(token,jwt_secret);
+            console.log(decode);
+            req.user=decode;
+            console.log(decode);
+
+            
+        } catch (error) {
+            return res.status(500).json(
+                {
+                    status:"unsuccessful",
+                    success:false,
+                    error:" token is invalid",
+                });
+        }
+          next();
+        
+    } catch (error) {
+        return res.status(500).json(
+            {
+                status:"unsuccessful",
+                success:false,
+                error:error,
+            });
+        
+    }
+
+}
+
+exports.isStudent=(req,res,next)=>
+{
+    try {
+        if(req.user.Role!=="Student")
+        {
+            
+         
+                return res.status(500).json(
+                    {
+                        status:"unsuccessful",
+                        success:false,
+                        error:" user not authorize for this role",
+                    });
+                    next();
+        }
+        
+    } catch (error) {
+        return res.status(500).json(
+            {
+                status:"unsuccessful",
+                success:false,
+                error:error,
+            });
+        
+    }
+
+}
+
+exports.isAdmin=(req,res,next)=>
+{
+    try {
+        if(req.user.Role!=="Admin")
+        {
+            return res.status(500).json(
+                {
+                    status:"unsuccessful",
+                    success:false,
+                    error:"user not authorize for this role",
+                });
+        }
+
+        
+    } catch (error) {
+        return res.status(500).json(
+            {
+                status:"unsuccessful",
+                success:false,
+                error:" token not found",
+            });
+        
+    }
+}
--- routes/user.js
+++ routes/user.js
@@ -5,16 +5,29 @@ const {signup}=require("../controllers/signup");
 const {Auth,isStudent,isAdmin}=require("../middlewares/auth")
 router.post("/login",login);
 router.post("/signup",signup);
-// router.get("/student",Auth,isStudent,(req,res)=>
-// {
-//     res.json(
-//         {
-//             status:"successful",
-//             isStudnent:true,
-//             mesage:"welcome to protected route of studnet",
+router.get("/student",Auth,isStudent,(req,res)=>
+{
+    res.json(
+        {
+            status:"successful",
+            isStudnent:true,
+            mesage:"welcome to protected route of studnet",
 
-//         }
-//     )
+        }
+    )
 
-// })
+})
+
+router.get("/admin",Auth,isAdmin,(req,res)=>
+{
+    res.json(
+        {
+            status:"successful",
+            isStudnent:true,
+            mesage:"welcome to protected route of Admin",
+
+        }
+    )
+
+})
 module.exports=router;
